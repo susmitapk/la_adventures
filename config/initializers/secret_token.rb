e0c28c79d99839e020d1300fff46e86bf5f5567c
Susmita Patnala <SusmitaPatnala@macbook86-2.local>
2015-10-23 20:58:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaAdventures::Application.config.secret_token = 'f5c37fd815d126c66f6ab4b301681ce75d41f3512b0bcb4a0dd22495a91a23fb89a804345c1ef0df1ba16e9b644d4bf8e26c62ef2615fbc010a140dfbfa81994'
